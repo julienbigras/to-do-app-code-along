970276385e24cb4382eb108f89cb3ca31baffa19
Julien <julien.bigras@hotmail.com>
2021-02-15 21:00:53 -0500

    just starting out

---
+++ src/testData.js
@@ -0,0 +1,16 @@
+const DATA = [
+  {
+    name: "task 1",
+    id: "task 1",
+  },
+  {
+    name: "task 2",
+    id: "task 2",
+  },
+  {
+    name: "task 3",
+    id: "task 3",
+  },
+];
+
+export default DATA;
