270a6c0e189273797f3d9253da6dcd8a356e1e45
julienbigras <julien.bigras@hotmail.com>
2021-03-09 17:03:12 -0500

    more event handling

--- src/App.js
+++ src/App.js
@@ -1,20 +1,47 @@
+import { useState } from "react";
+import { nanoid } from "nanoid";
 import Todo from "./Components/Todo";
 import Form from "./Components/Form";
 import FilterButton from "./Components/FilterButton";
 
 function App(props) {
+  // setState for our task list
+  const [tasks, setTasks] = useState(props.tasks);
+
+  // function to add a new task to the to do list
   function addTask(name) {
-    alert(`hello this is ${name}`);
+    const newTask = {
+      id: `todo-${nanoid()}`,
+      name: name,
+      completed: false,
+    };
+    setTasks([...tasks, newTask]);
   }
 
-  const taskList = props.tasks.map((task) => (
+  // function to update the "completed" value of the task object in state
+  function toggleCompletedTask(id) {
+    const updatedTasks = tasks.map((task) => {
+      if (id === task.id) {
+        return { ...task, completed: !task.completed };
+      }
+      return task;
+    });
+    setTasks(updatedTasks);
+  }
+
+  // maps over the starting tasks data being passed into the App component as a prop
+  const taskList = tasks.map((task) => (
     <Todo
       name={task.name}
       completed={task.completed}
       id={task.id}
       key={task.id}
+      toggleCompletedTask={toggleCompletedTask}
     />
   ));
+
+  const tasksNoun = tasks.length !== 1 ? "tasks" : "task";
+  const headingText = `${tasks.length} ${tasksNoun} remaining`;
 
   return (
     <div className="todoapp stack-large">
@@ -25,7 +52,7 @@ function App(props) {
         <FilterButton />
         <FilterButton />
       </div>
-      <h2 id="list-heading">3 tasks remaining</h2>
+      <h2 id="list-heading">{headingText}</h2>
       <ul
         role="list"
         className="todo-list stack-large stack-exception"
